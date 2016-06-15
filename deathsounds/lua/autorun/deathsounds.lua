c5bc7f976377804e95ed16392ac2a56c203d1d3f
Michael S <OctothorpeObelus@users.noreply.github.com>
2016-06-15 18:06:42 -0400

    Update deathsounds.lua

--- deathsounds/lua/autorun/deathsounds.lua
+++ deathsounds/lua/autorun/deathsounds.lua
@@ -2,7 +2,7 @@
 function DeathSounds(victim, weapon, killer)
 	local maxrand = 0 --Change this 59 to the largest number in your sounds folder Ex. death68.wav would be 68
 	local soundrand = math.random(1,maxrand)
-	local sound = "deathsound/death" .. soundrand .. ".wav"
+	local sound = "deathsound/death" .. soundrand .. ".wav" --Replace .wav with .mp3 or .ogg depending.
 	
 	util.PrecacheSound(sound)
 	victim:EmitSound(sound, 72, 100)
