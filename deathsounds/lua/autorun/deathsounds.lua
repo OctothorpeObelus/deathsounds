0d9452857155bd45cd3f8ec807dd351c10aa3890
Michael S <OctothorpeObelus@users.noreply.github.com>
2018-07-16 14:18:57 -0400

    Adjust to conform to latest workshop update
    Fixed soundLevel not being 100
    Default format is .mp3, but can be easily changed
    UID'ed hooks to prevent conflicts

--- deathsounds/lua/autorun/deathsounds.lua
+++ deathsounds/lua/autorun/deathsounds.lua
@@ -2,15 +2,15 @@
 function DeathSounds(victim, weapon, killer)
 	local maxrand = 0 --Change this 59 to the largest number in your sounds folder Ex. death68.wav would be 68
 	local soundrand = math.random(1,maxrand)
-	local sound = "deathsound/death" .. soundrand .. ".wav" --Replace .wav with .mp3 or .ogg depending.
+	local sound = "deathsound/death" .. soundrand .. ".mp3" --Replace .wav with .mp3 or .ogg depending.
 	
 	util.PrecacheSound(sound)
-	victim:EmitSound(sound, 72, 100)
+	victim:EmitSound(sound, 100, 100)
 end
-hook.Add( "PlayerDeath", "DeathSoundsHook", DeathSounds )
+hook.Add( "PlayerDeath", "RandomDeathSounds", DeathSounds )
 
 -- Don't change anything down here
 function RemoveFlatline()
 return true
 end
-hook.Add( "PlayerDeathSound", "RemoveFlatlineSound", RemoveFlatline )
+hook.Add( "PlayerDeathSound", "RandomDeathSounds_RemoveFlatlineSound", RemoveFlatline )
